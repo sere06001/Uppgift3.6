5999b59bb4a75c910d8974452384af8a26167238
sere06001 <sere06001@utb.vaxjo.se>
2023-10-07 01:45:38 +0200

    Add project files.

---
+++ Uppgift 3.6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Uppgift3_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ditt förnamn och efternamn.");
+            string helanamn = Console.ReadLine();
+            int mellanslagIndex = helanamn.IndexOf(" ");
+            string förnamn = helanamn[..mellanslagIndex];
+            string efternamn = helanamn[(mellanslagIndex + 1)..];
+
+            if (string.Compare(förnamn, efternamn, StringComparison.CurrentCulture) == -1)
+            {
+                Console.WriteLine("Ditt förnamn kommer först i bokstavsordning.");
+            }
+            else if (string.Compare(förnamn, efternamn, StringComparison.CurrentCulture) == 0)
+            {
+                Console.WriteLine("Ditt förnamn och efternamn ligger på samma plats i bokstavs ordning.");
+            }
+            else if (string.Compare(förnamn, efternamn, StringComparison.CurrentCulture) == 1)
+            {
+                Console.WriteLine("Ditt efternamn kommer först i bokstavsorndning.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
